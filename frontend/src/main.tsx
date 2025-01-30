23664f07950f80bbddf43c8d94e38f75e684040f
Manohar Kale <manoharkale5368@gmail.com>
2025-01-30 18:19:02 +0530

    created front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { AuthProvider } from './components/auth/AuthProvider';
+import { AuthProvider } from './components/auth/AuthProvider.tsx';
 import App from './App.tsx';
 import './index.css';
 
