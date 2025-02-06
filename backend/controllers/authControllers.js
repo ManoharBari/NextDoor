8acd9abf05029d48e570102f4d7b13d724557072
Manohar Kale <manoharkale5368@gmail.com>
2025-02-06 15:56:49 +0530

    created routes

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// User Signup
+const signup = async (req, res) => {
+  try {
+    const { name, email, password, role, location, skills } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+      location,
+      skills,
+    });
+    res.status(201).json({ user, message: "User Signup successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Internal Server Error" });
+  }
+};
+
+// User Login
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.json({ token, user, message: "User Login successfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = { signup, login };
