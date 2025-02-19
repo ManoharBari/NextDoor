a68b10d810816e2e9a1c0c620f4861f17a6e89da
Manohar Kale <manoharkale5368@gmail.com>
2025-02-19 19:19:27 +0530

    created order context api

--- frontend/src/context/order/orderState.jsx
+++ frontend/src/context/order/orderState.jsx
@@ -5,23 +5,42 @@ import OrderContext from "./orderContext";
 
 function orderState({ children }) {
     const host = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}`;
-    const [orderData, setOrderData] = useState([{
-        _id: "67b2b9e9dc7a9990221d8be8",
-        userId: {
-            _id: "67a49395f327929d732a51a3",
-            name: "Manohar Kale",
-            profilePicture: ""
+    const [orderData, setOrderData] = useState([
+        {
+            _id: "67b2b9e9dc7a9990221d8be8",
+            userId: {
+                _id: "67a49395f327929d732a51a3",
+                name: "Manohar Kale",
+                profilePicture: ""
+            },
+            serviceId: {
+                _id: "67b170486c3b8ad97ba4c1e6",
+                title: "Profesional Plumbing Services",
+                description: "I can solve any Plumbing problem"
+            },
+            amount: 299,
+            currency: "INR",
+            status: "created",
+            createdAt: "2025-02-19T13:42:22.898Z"
         },
-        serviceId: {
-            _id: "67b170486c3b8ad97ba4c1e6",
-            title: "Profesional Plumbing Services",
-            description: "I can solve any Plumbing problem"
+        {
+            _id: "67b2b9e9dc7a9990221d8be8",
+            userId: {
+                _id: "67a49395f327929d732a51a3",
+                name: "Manohar Kale",
+                profilePicture: ""
+            },
+            serviceId: {
+                _id: "67b170486c3b8ad97ba4c1e6",
+                title: "Profesional Plumbing Services",
+                description: "I can solve any Plumbing problem"
+            },
+            amount: 299,
+            currency: "INR",
+            status: "created",
+            createdAt: "2025-02-19T13:42:22.898Z"
         },
-        amount: 299,
-        currency: "INR",
-        status: "created",
-        createdAt: "2025-02-19T13:42:22.898Z"
-    },]);
+    ]);
     const { user } = useContext(UserContext);
 
     // Show All Orders
